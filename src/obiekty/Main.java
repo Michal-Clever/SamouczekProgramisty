78da8570d55a50268e1f3099545d7069dbfe5ef9
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-11-12 15:12:16 +0100

    SamouczekProgramisty

--- src/obiekty/Main.java
+++ src/obiekty/Main.java
@@ -1,4 +1,21 @@
 package obiekty;
 
+import obiekty.bird.Eagle;
+import obiekty.bird.Stock;
+import obiekty.fish.Carp;
+import obiekty.fish.GoldFish;
+
 public class Main {
+    public static void main(String[] args) {
+
+        GoldFish lola = new GoldFish(1);
+        Carp danek = new Carp(35);
+        Eagle strzala = new Eagle("braun");
+        Stock cwir = new Stock(3);
+
+        System.out.println(lola.getWeight());
+        System.out.println(lola.isBig(1));
+
+    }
+
 }
--- src/obiekty/fish/GoldFish.java
+++ src/obiekty/fish/GoldFish.java
@@ -3,11 +3,22 @@ package obiekty.fish;
 public class GoldFish {
     private int weight;
 
-    public void setWeight(int weight) {
-        this.weight = weight;
+    public int getWeight() {
+        return weight;
     }
 
     public GoldFish(int weight) {
         this.weight = weight;
+    }
+
+    public boolean isBig(int weight) {
+        boolean isPositive = weight > 5;
+        if (isPositive) {
+            System.out.println("Duza sztuka");
+        } else {
+            System.out.println("Norma");
+        }
+
+        return isPositive;
     }
 }
